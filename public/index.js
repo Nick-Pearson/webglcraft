ed7dedaca25796e4eaed104f44ff5eb3eba52521
Nick Pearson <nicholas.pearson4@btinternet.com>
2020-03-01 11:24:38 +0000

    Setup package.json and linting rules

--- public/index.js
+++ public/index.js
@@ -1,63 +1,65 @@
-import RenderEngine from './renderengine/index.js'
-import TextureLoader from './textureloader/index.js'
+import RenderEngine from './renderengine/index.js';
+import TextureLoader from './textureloader/index.js';
 
 let time;
 
 //
 // start here
 //
-function main() {
-    const canvas = document.querySelector("#glCanvas");
-    const fpsCounter = document.querySelector("#fpsCounter");
+function main()
+{
+  const canvas = document.querySelector('#glCanvas');
+  const fpsCounter = document.querySelector('#fpsCounter');
 
-    // Initialize the GL context
-    const gl = canvas.getContext("webgl");
-    const renderer = new RenderEngine(gl);
-    const textureLoader = new TextureLoader(gl);
-  
-    // Only continue if WebGL is available and working
-    if (gl === null) {
-      alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-      return;
-    }
-    
-    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-    const texture = textureLoader.loadTexture('assets/textures/block/oak_log_top.png');
-  
-    const shader = {
-        program: shaderProgram,
-        attribLocations: {
-          vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
-          textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
-        },
-        uniformLocations: {
-          projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
-          modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
-          uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
-        },
-        texture: texture,
-      };
+  // Initialize the GL context
+  const gl = canvas.getContext('webgl');
+  const renderer = new RenderEngine(gl);
+  const textureLoader = new TextureLoader(gl);
 
-    const square =  initBuffers(gl);
-    renderer.addObject(square, shader);
+  // Only continue if WebGL is available and working
+  if (gl === null)
+  {
+    alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+    return;
+  }
 
-    let last = 0;
-    time = 0;
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+  const texture = textureLoader.loadTexture('assets/textures/block/oak_log_top.png');
 
-    function render(now)
-    {
-        renderer.drawScene(time);
+  const shader = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+      uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
+    },
+    texture: texture,
+  };
 
-        const frameTime = (now - last).toFixed(2);
-        const fps = (1000 / (now - last)).toFixed(0);
-        time += (now - last) / 1000;
-        fpsCounter.textContent = fps + " FPS ( " + frameTime + "ms )";
+  const square = initBuffers(gl);
+  renderer.addObject(square, shader);
 
-        last = now;
-        requestAnimationFrame(render);
-    }
+  let last = 0;
+  time = 0;
+
+  function render(now)
+  {
+    renderer.drawScene(time);
+
+    const frameTime = (now - last).toFixed(2);
+    const fps = (1000 / (now - last)).toFixed(0);
+    time += (now - last) / 1000;
+    fpsCounter.textContent = fps + ' FPS ( ' + frameTime + 'ms )';
+
+    last = now;
     requestAnimationFrame(render);
   }
+  requestAnimationFrame(render);
+}
 
 window.onload = main;
 
@@ -94,177 +96,181 @@ const fsSource = `
 //
 // Initialize a shader program, so WebGL knows how to draw our data
 //
-function initShaderProgram(gl, vsSource, fsSource) {
-    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-  
-    // Create the shader program
-  
-    const shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-  
-    // If creating the shader program failed, alert
-  
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-      alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
-      return null;
-    }
-  
-    return shaderProgram;
-  }
-  
-  //
-  // creates a shader of the given type, uploads the source and
-  // compiles it.
-  //
-  function loadShader(gl, type, source) {
-    const shader = gl.createShader(type);
-  
-    // Send the source to the shader object
-  
-    gl.shaderSource(shader, source);
-  
-    // Compile the shader program
-  
-    gl.compileShader(shader);
-  
-    // See if it compiled successfully
-  
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
-      gl.deleteShader(shader);
-      return null;
-    }
-  
-    return shader;
+function initShaderProgram(gl, vsSource, fsSource)
+{
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  // Create the shader program
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  // If creating the shader program failed, alert
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS))
+  {
+    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    return null;
   }
 
-  function initBuffers(gl) {
+  return shaderProgram;
+}
 
-    // Create a buffer for the square's positions.
-  
-    const positionBuffer = gl.createBuffer();
-  
-    // Select the positionBuffer as the one to apply buffer
-    // operations to from here out.
-  
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  
-    // Now create an array of positions for the square.
-  
-    const positions = [
-        // Front face
-        -1.0, -1.0,  1.0,
-         1.0, -1.0,  1.0,
-         1.0,  1.0,  1.0,
-        -1.0,  1.0,  1.0,
-        
-        // Back face
-        -1.0, -1.0, -1.0,
-        -1.0,  1.0, -1.0,
-         1.0,  1.0, -1.0,
-         1.0, -1.0, -1.0,
-        
-        // Top face
-        -1.0,  1.0, -1.0,
-        -1.0,  1.0,  1.0,
-         1.0,  1.0,  1.0,
-         1.0,  1.0, -1.0,
-        
-        // Bottom face
-        -1.0, -1.0, -1.0,
-         1.0, -1.0, -1.0,
-         1.0, -1.0,  1.0,
-        -1.0, -1.0,  1.0,
-        
-        // Right face
-         1.0, -1.0, -1.0,
-         1.0,  1.0, -1.0,
-         1.0,  1.0,  1.0,
-         1.0, -1.0,  1.0,
-        
-        // Left face
-        -1.0, -1.0, -1.0,
-        -1.0, -1.0,  1.0,
-        -1.0,  1.0,  1.0,
-        -1.0,  1.0, -1.0,
-      ];
-  
-    // Now pass the list of positions into WebGL to build the
-    // shape. We do this by creating a Float32Array from the
-    // JavaScript array, then use it to fill the current buffer.
-  
-    gl.bufferData(gl.ARRAY_BUFFER,
-                  new Float32Array(positions),
-                  gl.STATIC_DRAW);
+//
+// creates a shader of the given type, uploads the source and
+// compiles it.
+//
+function loadShader(gl, type, source)
+{
+  const shader = gl.createShader(type);
 
-    const indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  // Send the source to the shader object
 
-    // This array defines each face as two triangles, using the
-    // indices into the vertex array to specify each triangle's
-    // position.
+  gl.shaderSource(shader, source);
 
-    const indices = [
-    0,  1,  2,      0,  2,  3,    // front
-    4,  5,  6,      4,  6,  7,    // back
-    8,  9,  10,     8,  10, 11,   // top
-    12, 13, 14,     12, 14, 15,   // bottom
-    16, 17, 18,     16, 18, 19,   // right
-    20, 21, 22,     20, 22, 23,   // left
-    ];
+  // Compile the shader program
 
-    // Now send the element array to GL
+  gl.compileShader(shader);
 
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-        new Uint16Array(indices), gl.STATIC_DRAW);
-  
-    const textureCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-  
-    const textureCoordinates = [
-      // Front
-      0.0,  0.0,
-      1.0,  0.0,
-      1.0,  1.0,
-      0.0,  1.0,
-      // Back
-      0.0,  0.0,
-      1.0,  0.0,
-      1.0,  1.0,
-      0.0,  1.0,
-      // Top
-      0.0,  0.0,
-      1.0,  0.0,
-      1.0,  1.0,
-      0.0,  1.0,
-      // Bottom
-      0.0,  0.0,
-      1.0,  0.0,
-      1.0,  1.0,
-      0.0,  1.0,
-      // Right
-      0.0,  0.0,
-      1.0,  0.0,
-      1.0,  1.0,
-      0.0,  1.0,
-      // Left
-      0.0,  0.0,
-      1.0,  0.0,
-      1.0,  1.0,
-      0.0,  1.0,
-    ];
-  
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
-                  gl.STATIC_DRAW);
+  // See if it compiled successfully
 
-    return {
-      verts: positionBuffer,
-      numVerts: positions.length,
-      tris: indexBuffer,
-      numTris: indices.length,
-      uvs: textureCoordBuffer,
-    };
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+  {
+    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
   }
+
+  return shader;
+}
+
+function initBuffers(gl)
+{
+  // Create a buffer for the square's positions.
+
+  const positionBuffer = gl.createBuffer();
+
+  // Select the positionBuffer as the one to apply buffer
+  // operations to from here out.
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // Now create an array of positions for the square.
+
+  const positions = [
+    // Front face
+    -1.0, -1.0, 1.0,
+    1.0, -1.0, 1.0,
+    1.0, 1.0, 1.0,
+    -1.0, 1.0, 1.0,
+
+    // Back face
+    -1.0, -1.0, -1.0,
+    -1.0, 1.0, -1.0,
+    1.0, 1.0, -1.0,
+    1.0, -1.0, -1.0,
+
+    // Top face
+    -1.0, 1.0, -1.0,
+    -1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, -1.0,
+
+    // Bottom face
+    -1.0, -1.0, -1.0,
+    1.0, -1.0, -1.0,
+    1.0, -1.0, 1.0,
+    -1.0, -1.0, 1.0,
+
+    // Right face
+    1.0, -1.0, -1.0,
+    1.0, 1.0, -1.0,
+    1.0, 1.0, 1.0,
+    1.0, -1.0, 1.0,
+
+    // Left face
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0, 1.0,
+    -1.0, 1.0, 1.0,
+    -1.0, 1.0, -1.0,
+  ];
+
+  // Now pass the list of positions into WebGL to build the
+  // shape. We do this by creating a Float32Array from the
+  // JavaScript array, then use it to fill the current buffer.
+
+  gl.bufferData(gl.ARRAY_BUFFER,
+      new Float32Array(positions),
+      gl.STATIC_DRAW);
+
+  const indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+  // This array defines each face as two triangles, using the
+  // indices into the vertex array to specify each triangle's
+  // position.
+
+  const indices = [
+    0, 1, 2, 0, 2, 3, // front
+    4, 5, 6, 4, 6, 7, // back
+    8, 9, 10, 8, 10, 11, // top
+    12, 13, 14, 12, 14, 15, // bottom
+    16, 17, 18, 16, 18, 19, // right
+    20, 21, 22, 20, 22, 23, // left
+  ];
+
+  // Now send the element array to GL
+
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
+      new Uint16Array(indices), gl.STATIC_DRAW);
+
+  const textureCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+
+  const textureCoordinates = [
+    // Front
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // Back
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // Top
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // Bottom
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // Right
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // Left
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+  ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
+      gl.STATIC_DRAW);
+
+  return {
+    verts: positionBuffer,
+    numVerts: positions.length,
+    tris: indexBuffer,
+    numTris: indices.length,
+    uvs: textureCoordBuffer,
+  };
+}
--- public/renderengine/index.js
+++ public/renderengine/index.js
@@ -1,140 +1,141 @@
 function RenderEngine(gl)
 {
-    console.log("Initialising RenderEngine");
-    this.gl = gl;
-    this.renderables = [];
-    this.projectionMatrix = createProjectionMatrix(gl, 45);
+  console.log('Initialising RenderEngine');
+  this.gl = gl;
+  this.renderables = [];
+  this.projectionMatrix = createProjectionMatrix(gl, 45);
 
-    initGL(gl);
+  initGL(gl);
 }
 
 function initGL(gl)
 {
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
-    gl.clearColor(0.0, 0.0, 0.2, 1.0);
-    gl.clearDepth(1.0);
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LEQUAL);
+  gl.clearColor(0.0, 0.0, 0.2, 1.0);
+  gl.clearDepth(1.0);
 }
 
 function createProjectionMatrix(gl, fov)
 {
-    const fieldOfView = fov * Math.PI / 180;
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.1;
-    const zFar = 100.0;
-    const projectionMatrix = mat4.create();
+  const fieldOfView = fov * Math.PI / 180;
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 100.0;
+  const projectionMatrix = mat4.create();
 
-    mat4.perspective(projectionMatrix,
-                        fieldOfView, 
-                        aspect,
-                        zNear,
-                        zFar);
+  mat4.perspective(projectionMatrix,
+      fieldOfView,
+      aspect,
+      zNear,
+      zFar);
 
-    return projectionMatrix;
+  return projectionMatrix;
 }
 
 RenderEngine.prototype.drawScene = function(time)
 {
-    const gl = this.gl;
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  const gl = this.gl;
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    this.renderables.forEach(renderable => {
-        this.draw(renderable, time);
-    });
+  this.renderables.forEach((renderable) =>
+  {
+    this.draw(renderable, time);
+  });
 };
 
 RenderEngine.prototype.draw = function(renderable, time)
 {
-    let gl = this.gl;
+  const gl = this.gl;
 
-    // Set the drawing position to the "identity" point, which is
-    // the center of the scene.
-    const modelViewMatrix = mat4.create();
+  // Set the drawing position to the "identity" point, which is
+  // the center of the scene.
+  const modelViewMatrix = mat4.create();
 
-    // Now move the drawing position a bit to where we want to
-    // start drawing the square.
+  // Now move the drawing position a bit to where we want to
+  // start drawing the square.
 
-    mat4.translate(modelViewMatrix,     // destination matrix
-                    modelViewMatrix,     // matrix to translate
-                    [-0.0, 0.0, -6.0]);  // amount to translate
+  mat4.translate(modelViewMatrix, // destination matrix
+      modelViewMatrix, // matrix to translate
+      [-0.0, 0.0, -6.0]); // amount to translate
 
-    mat4.rotate(modelViewMatrix, modelViewMatrix, time, [1, 0, 0]);
-    mat4.rotate(modelViewMatrix, modelViewMatrix, time * .7, [0, 1, 0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, time, [1, 0, 0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, time * .7, [0, 1, 0]);
 
-    // Tell WebGL how to pull out the positions from the position
-    // buffer into the vertexPosition attribute.
-    {
-        const numComponents = 3;  // pull out 2 values per iteration
-        const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-        const normalize = false;  // don't normalize
-        const stride = 0;         // how many bytes to get from one set of values to the next
-                                // 0 = use type and numComponents above
-        const offset = 0;         // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, renderable.mesh.verts);
-        gl.vertexAttribPointer(
-            renderable.shader.attribLocations.vertexPosition,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(
-            renderable.shader.attribLocations.vertexPosition);
-    }
+  // Tell WebGL how to pull out the positions from the position
+  // buffer into the vertexPosition attribute.
+  {
+    const numComponents = 3; // pull out 2 values per iteration
+    const type = gl.FLOAT; // the data in the buffer is 32bit floats
+    const normalize = false; // don't normalize
+    const stride = 0; // how many bytes to get from one set of values to the next
+    // 0 = use type and numComponents above
+    const offset = 0; // how many bytes inside the buffer to start from
+    gl.bindBuffer(gl.ARRAY_BUFFER, renderable.mesh.verts);
+    gl.vertexAttribPointer(
+        renderable.shader.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        renderable.shader.attribLocations.vertexPosition);
+  }
 
-    {
-      // Tell WebGL which indices to use to index the vertices
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, renderable.mesh.tris);
-    }
+  {
+    // Tell WebGL which indices to use to index the vertices
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, renderable.mesh.tris);
+  }
 
-    // tell webgl how to pull out the texture coordinates from buffer
-    {
-        const num = 2; // every coordinate composed of 2 values
-        const type = gl.FLOAT; // the data in the buffer is 32 bit float
-        const normalize = false; // don't normalize
-        const stride = 0; // how many bytes to get from one set to the next
-        const offset = 0; // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, renderable.mesh.uvs);
-        gl.vertexAttribPointer(renderable.shader.attribLocations.textureCoord, num, type, normalize, stride, offset);
-        gl.enableVertexAttribArray(renderable.shader.attribLocations.textureCoord);
-    }
+  // tell webgl how to pull out the texture coordinates from buffer
+  {
+    const num = 2; // every coordinate composed of 2 values
+    const type = gl.FLOAT; // the data in the buffer is 32 bit float
+    const normalize = false; // don't normalize
+    const stride = 0; // how many bytes to get from one set to the next
+    const offset = 0; // how many bytes inside the buffer to start from
+    gl.bindBuffer(gl.ARRAY_BUFFER, renderable.mesh.uvs);
+    gl.vertexAttribPointer(renderable.shader.attribLocations.textureCoord, num, type, normalize, stride, offset);
+    gl.enableVertexAttribArray(renderable.shader.attribLocations.textureCoord);
+  }
 
-    // Tell WebGL to use our program when drawing
-    gl.useProgram(renderable.shader.program);
+  // Tell WebGL to use our program when drawing
+  gl.useProgram(renderable.shader.program);
 
-    // Tell WebGL we want to affect texture unit 0
-    gl.activeTexture(gl.TEXTURE0);
+  // Tell WebGL we want to affect texture unit 0
+  gl.activeTexture(gl.TEXTURE0);
 
-    // Bind the texture to texture unit 0
-    gl.bindTexture(gl.TEXTURE_2D, renderable.shader.texture);
+  // Bind the texture to texture unit 0
+  gl.bindTexture(gl.TEXTURE_2D, renderable.shader.texture);
 
-    // Tell the shader we bound the texture to texture unit 0
-    gl.uniform1i(renderable.shader.uniformLocations.uSampler, 0);
+  // Tell the shader we bound the texture to texture unit 0
+  gl.uniform1i(renderable.shader.uniformLocations.uSampler, 0);
 
-    // Set the shader uniforms
-    gl.uniformMatrix4fv(
-        renderable.shader.uniformLocations.projectionMatrix,
-        false,
-        this.projectionMatrix);
-    gl.uniformMatrix4fv(
-        renderable.shader.uniformLocations.modelViewMatrix,
-        false,
-        modelViewMatrix);
+  // Set the shader uniforms
+  gl.uniformMatrix4fv(
+      renderable.shader.uniformLocations.projectionMatrix,
+      false,
+      this.projectionMatrix);
+  gl.uniformMatrix4fv(
+      renderable.shader.uniformLocations.modelViewMatrix,
+      false,
+      modelViewMatrix);
 
-    {
-        const vertexCount = renderable.mesh.numTris;
-        const type = gl.UNSIGNED_SHORT;
-        const offset = 0;
-        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
-    }
-}
+  {
+    const vertexCount = renderable.mesh.numTris;
+    const type = gl.UNSIGNED_SHORT;
+    const offset = 0;
+    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+  }
+};
 
 RenderEngine.prototype.addObject = function(mesh, shader)
 {
-    this.renderables.push({
-        mesh: mesh,
-        shader: shader
-    });
-}
+  this.renderables.push({
+    mesh: mesh,
+    shader: shader,
+  });
+};
 
-export { RenderEngine as default };
+export {RenderEngine as default};
